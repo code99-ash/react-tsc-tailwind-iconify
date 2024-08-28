66f41377681b1a9c4f5197d03f195a420daf961a
code99-ash <oyetikh1@gmail.com>
2024-08-28 07:05:24 +0000

    tailwind properly configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Icon } from '@iconify/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="">
+      <header className="text-2xl flex items-center gap-3 justify-center">
+        <Icon icon="mdi-light:home" />
+        <h1 className="text-rose-600">Home</h1>
       </header>
     </div>
   );
